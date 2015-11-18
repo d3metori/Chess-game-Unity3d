f8881f7b3a9cca4c999a3cbb851083b822c3c54a
6darkersol9 <gaaradim@yandex.ru>
2015-11-18 03:31:35 +0300

    03

--- Assets/Scripts/figure.cs
+++ Assets/Scripts/figure.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class figure
 {
     public int[] position;  // позиция данной фигуры где [1] - x(строка)   [2] - z(столбец)
-    public bool color;  //true - white. black - false
-
+    public enum colors { white = 1, black = 2 };    // дабы не считать с нуля
+    public colors colors_of_figure;
 
 }
--- Assets/Scripts/square.cs
+++ Assets/Scripts/square.cs
@@ -4,34 +4,37 @@ using System.Collections;
 public class square : MonoBehaviour {
 
 
-        public int[] position;  // позиция данной клетки где [1] - x(строка)   [2] - z(столбец)
-
+        public int[] position;  // позиция данной клетки где [0] - x(строка)   [1] - z(столбец)
         public bool Empty;
-        public enum Components { First, Second};
-        public Components zz;
+        public enum colors { white = 1, black = 2 };    // дабы не считать с нуля
+        public colors colors_of_square;
 
+        public Material[] materials;    // 1 - white 2 - black
+        public Renderer rend;
 
-        Components ReverseDirection(Components dir)
+        colors ReverseDirection(colors dir)
         {
-            if (dir == Components.First)
-                dir = Components.First;
-            else if (dir == Components.Second)
-                dir = Components.Second;
+            if (colors_of_square == colors.white){
+                rend.sharedMaterial = materials[0];
+              //  Debug.Log("changed to mat [0]");
+            }
 
-            return dir;
+            if (colors_of_square == colors.black) {
+                rend.sharedMaterial = materials[1];
+               // Debug.Log("changed to mat [1]");
+            }
+             
+                return dir;
         }
 
        
     void Start(){
-         //figure = Components.First;
-         ReverseDirection(zz);
-         Debug.Log(zz);
 
+         ReverseDirection(colors_of_square);
          figure fiq = new figure();
-         fiq.color = true;
-         Debug.Log(fiq.color);
+
+         fiq.colors_of_figure = 0;
+         Debug.Log(fiq.colors_of_figure);
 
      }
-    
-
    }
